4e6e93871135c3ed88e481ebcb68de0108099119
leedm <leedm68@gmail.com>
2022-11-13 01:38:49 +0900

    데이타베이스 환경설정

--- src/test/java/com/example/springbootjpa01/SpringbootJpa01ApplicationTests.java
+++ src/test/java/com/example/springbootjpa01/SpringbootJpa01ApplicationTests.java
@@ -1,11 +1,13 @@
 package com.example.springbootjpa01;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootJpa01ApplicationTests {
 
+    @Disabled
     @Test
     void contextLoads() {
     }
